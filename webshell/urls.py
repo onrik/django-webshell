aca6be43ecef7c0a893c127c592f04dbb816f623
Andrey <and@rey.im>
2016-04-21 09:15:53 +0300

    Fix Django 1.9 warnings.

--- webshell/urls.py
+++ webshell/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from .views import execute_script_view
 
-urlpatterns = patterns('',
-    url(r'^execute/$', execute_script_view, name='execute-script'),
-)
+urlpatterns = [
+    url(r'^execute/$', execute_script_view, name='execute'),
+]
