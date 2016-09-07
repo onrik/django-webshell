ba45d5885713e0cc7beef54ade4eb627e6418244
Andrey <and@rey.im>
2016-09-07 16:59:12 +0300

    Tests file.

---
+++ runtests.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+import sys
+
+import django
+from django.conf import settings
+from django.test.utils import get_runner
+
+
+if not settings.configured:
+    settings.configure(
+        DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': ':memory:',
+                'USER': '',
+                'PASSWORD': '',
+                'HOST': '',
+                'PORT': '',
+            }
+        },
+
+        INSTALLED_APPS=[
+            'django.contrib.admin',
+            'django.contrib.auth',
+            'django.contrib.contenttypes',
+            'django.contrib.sessions',
+            'django.contrib.messages',
+            'django.contrib.staticfiles',
+
+            'webshell',
+        ],
+        MIDDLEWARE_CLASSES = [
+            'django.middleware.security.SecurityMiddleware',
+            'django.contrib.sessions.middleware.SessionMiddleware',
+            'django.middleware.common.CommonMiddleware',
+            'django.middleware.csrf.CsrfViewMiddleware',
+            'django.contrib.auth.middleware.AuthenticationMiddleware',
+            'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+            'django.contrib.messages.middleware.MessageMiddleware',
+            'django.middleware.clickjacking.XFrameOptionsMiddleware',
+        ],
+
+        ROOT_URLCONF='webshell.urls',
+        DEBUG=False,
+    )
+
+
+def runtests():
+    django.setup()
+    TestRunner = get_runner(settings)
+    test_runner = TestRunner(verbosity=1, interactive=True)
+    
+    sys.exit(test_runner.run_tests([]))
+
+
+if __name__ == '__main__':
+    runtests()
